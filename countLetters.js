3a63338f7af214e9170b9dfc5ed619dd8ba06246
Frank <frankzou2000@hotmail.com>
2019-09-18 00:42:14 +0000

    added countletters

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u274C\u274C\u274C Assertion Failed: ${actual} !== ${expected}`);
+  }
+  return;
+};
+
+const countLetters = function(strToCount) {
+  let ret = {};
+  let noSpace = strToCount.split(" ").join("").split("");
+  for (const ele of noSpace) {
+    if (ret[ele]) {
+      ret[ele] += 1;
+    } else {
+      ret[ele] = 1;
+    }
+  }
+  return ret;
+};
+
+assertEqual(countLetters("lighthouse")["h"], 2);
+console.log(countLetters("lighthouse in the house"));
