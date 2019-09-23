12b15a57a9adaeaf5fa6f89a6e238c54f5d40ebc
Frank <frankzou2000@hotmail.com>
2019-09-23 17:40:43 +0000

    test

--- assertEqual.js
+++ assertEqual.js
@@ -8,8 +8,4 @@ const assertEqual = function(actual, expected) {
   return;
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 5);
+module.exports = assertEqual;
