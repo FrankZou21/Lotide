214e6e167258771db55fdfd2bdd8baad861aad81
Frank <frankzou2000@hotmail.com>
2019-09-23 21:31:13 +0000

    added mocha and chai functionality to middletest with Gabi

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,31 @@
-const assertArraysEqual = require("../assertArraysEqual");
+// const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
+const assert = require("chai").assert;
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([0, 1]), []);
-assertArraysEqual(middle([0, 1, 2]), [1]);
-assertArraysEqual(middle([0, 1, 2, 3]), [1, 2]);
-assertArraysEqual(middle([0, 1, 2, 3, 7, 8, 9, 0]), [3, 7]);
+describe("#middle", () => {
+  it("middle of 1 is []", () => {
+    let input = [1];
+    assert.deepEqual(middle(input), []);
+  })
+  it("middle of 0, 1 is []", () => {
+    let input = [0, 1];
+    assert.deepEqual(middle(input), []);
+  })
+  it("middle of 0, 1, 2 is 1", () => {
+    let input = [0, 1, 2];
+    assert.deepEqual(middle(input), [1]);
+  })
+  it("middle of 0, 1, 2, 3 is 1, 2", () => {
+    let input = [0, 1, 2, 3];
+    assert.deepEqual(middle(input), [1, 2]);
+  })
+  it("middle of 0, 1, 2, 3, 7, 8, 9, 0 is 3, 7", () => {
+    let input = [0, 1, 2, 3, 7, 8, 9, 0];
+    assert.deepEqual(middle(input), [3, 7]);
+  })
+})
+// assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([0, 1]), []);
+// assertArraysEqual(middle([0, 1, 2]), [1]);
+// assertArraysEqual(middle([0, 1, 2, 3]), [1, 2]);
+// assertArraysEqual(middle([0, 1, 2, 3, 7, 8, 9, 0]), [3, 7]);
