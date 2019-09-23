2b5ebb774723d35117bc014243fa686654d2dc12
Frank <frankzou2000@hotmail.com>
2019-09-23 20:42:26 +0000

    fixed head/added mocha and chai/ implemented mocha and chai in headtest with Gabi

--- head.js
+++ head.js
@@ -5,9 +5,4 @@ const head = function(arr){
   return arr[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([], undefined));
-
 module.exports = head;
--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,27 @@
 const head = require("../head");
-const assertEqual = require("../assertEqual");
+// const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([], undefined));
+describe("#head", () => {
+  it("returns head of [5, 6, 7] which is 5", () => {
+    let input = [5, 6, 7];
+    assert.strictEqual(head(input), 5);
+  });
+  it("returns head of [Hello, Lighthouse, Labs] which is Hello", () => {
+    let input = ["Hello", "Lighthouse", "Labs"];
+    assert.strictEqual(head(input), "Hello");
+  })
+  it("returns head of [1] which is 1", () => {
+    let input = [1];
+    assert.strictEqual(head(input), 1);
+  })
+  it("returns head of [] which is undefined", () => {
+    let input = [];
+    assert.strictEqual(head(input), undefined);
+  })
+});
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([1]), 1);
+// assertEqual(head([], undefined));
