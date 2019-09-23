d991a7d565544d59347a709b7935e00db6bfb9a1
Frank <frankzou2000@hotmail.com>
2019-09-23 21:14:06 +0000

    fixed tail function and added mocha and chai functions to tailtest with Gabi

--- tail.js
+++ tail.js
@@ -1,5 +1,9 @@
 const tail = function(arr) {
-  return arr.slice(1);
+  if(arr.length > 1){
+    return arr.slice(1);
+  } else {
+    return undefined;
+  }
 };
 
 module.exports = tail;
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,28 @@
-const assertEqual = require("../assertEqual");
+//const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const tail = require("../tail");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it("The tail of ['Hello', 'Lighthouse', 'Labs'] is ['Lighthouse', 'Labs']", () => {
+    let input = ["Hello", "Lighthouse", "Labs"];
+      assert.deepEqual(tail(input), ["Lighthouse", "Labs"]);
+  })
+  it("The tail of ['Hi'] is []", () => {
+    let input = ["Hi"];
+    assert.deepEqual(tail(input), undefined);
+  });
+});
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
 
-const results = tail(["hi"]);
-assertEqual(results[0], undefined);
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// // Test Case: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// const results = tail(["hi"]);
+// assertEqual(results[0], undefined);
